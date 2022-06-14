7a65f34eb60afc807219958419534c88595eb7a3
Sage-zhang <840925471@qq.com>
2022-06-14 15:00:35 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
+/**
+ * 项目的启动入口
+ */
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+// 加载全局样式文件
+import './styles/index.less'
+
+// 创建Vue根实例
+// 把router配置到根实例中
+// 通过render方法把APP根组件渲染到#app入口节点
 
 createApp(App).use(router).mount('#app')
